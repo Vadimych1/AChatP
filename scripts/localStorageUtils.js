19a9c7755ef1967e2fcce4c6eb8d20463b20197e
Vadimych1 <vadimlebdenko0@gmail.com>
2024-03-03 11:56:05 +0300

    update

--- scripts/localStorageUtils.js
+++
@@ -1,9 +0,0 @@
-// Функция для удаления элемента из localStorage по ключу
-function removeItemFromLocalStorage(key) {
-    localStorage.removeItem(key);
-}
-
-// Функция для очистки всех данных из localStorage
-function clearLocalStorage() {
-    localStorage.clear();
-}
---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const chatWindow = document.getElementById("chat-window");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+
+let messages = [];
+
+sendButton.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const message = messageInput.value;
+    const authcode = getCookie("auth_code");
+
+    if (message === "") {
+        var popup = document.createElement("div");
+        popup.classList.add("popup");
+
+        var text = document.createElement("p");
+        text.textContent = "Сообщение не может быть пустым!";
+
+        popup.appendChild(text);
+
+        popup.style.top = "-100px";
+
+        document.body.append(popup);
+
+        setTimeout(function () {
+            popup.style.top = "20px";
+        }, 20);
+
+        setTimeout(function () {
+            popup.style.top = "-100px";
+
+            setTimeout(function () {
+                popup.remove();    
+            }, 400);
+        }, 3000);
+    } else {
+        sendMessage(message, authcode == "" ? "null" : authcode)
+    }
+
+    messageInput.value = "";
+});
